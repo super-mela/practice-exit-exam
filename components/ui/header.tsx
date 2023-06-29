5d98168b4e91849001024a1e3a57ac22254daedd
melaku <genuieman@gmail.com>
2023-06-29 17:23:28 +0300

    user name on header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -10,14 +10,25 @@ import MobileMenu from './mobile-menu'
 export default function Header() {
 
   const [top, setTop] = useState<boolean>(true)
+  const [token, setToken] = useState<boolean>(false)
+  const [user, setUser] = useState<any>()
 
   // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
     window.pageYOffset > 10 ? setTop(false) : setTop(true)
   }
+  const userState = () => {
+    const tokenState = localStorage.getItem('token')
+    if (tokenState) {
+      setToken(true);
+      const userInfo = JSON.parse(localStorage.getItem('user') as typeof user);
+      setUser(userInfo);
+    }
+  }
 
   useEffect(() => {
     scrollHandler()
+    userState()
     window.addEventListener('scroll', scrollHandler)
     return () => window.removeEventListener('scroll', scrollHandler)
   }, [top])
@@ -36,17 +47,19 @@ export default function Header() {
           <nav className="hidden md:flex md:grow">
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
-              <li>
-                <Link href="/signin" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
-              </li>
-              <li>
-                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
-                </Link>
-              </li>
+              {token ?
+                <li>
+                  <Link href="/profile" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">{user?.userName}</Link>
+                </li>
+                : <li>
+                  <Link href="/signin" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                    <span>Sign in</span>
+                    <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                    </svg>
+                  </Link>
+                </li>}
+
             </ul>
 
           </nav>
